7258619994f326b5934b30e4031bf92581a3f59d
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2014-07-11 17:00:34 -0700

    RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,8 @@
 
 import sys
 import os
-
+# sets the default settings module for read the docs
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BE.settings.main")
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
