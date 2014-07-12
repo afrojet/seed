9102e10fdd4bd29d504c843a2ad1a2aa0d9e4eb6
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2014-07-11 17:03:11 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 import sys
 import os
 # sets the default settings module for read the docs
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BE.settings.main")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BE.settings.dev")
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
